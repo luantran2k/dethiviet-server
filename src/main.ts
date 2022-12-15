b6dc20eb751a6525e9fc482b4381eb48a0812c95
luantran2k <tranluan2kyl@gmail.com>
2022-12-15 19:30:57 +0700

    add ftp

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { join } from 'path';
 import { AppModule } from './app.module';
+import * as serveIndex from 'serve-index';
+import express from 'express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
   const PORT = process.env.PORT || 3000;
+  // app.useStaticAssets();
+  // const index = serveIndex('uploads', { icons: true });
+  app.useStaticAssets('uploads');
+  app.use('/uploads', serveIndex('uploads', { icons: true }));
 
   const config = new DocumentBuilder()
     .setTitle('Exam')
